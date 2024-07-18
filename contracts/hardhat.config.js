78998d04f384c28b2d8926a4ded727d45bd84448
Alex Astro <101653301+alexastro01@users.noreply.github.com>
2024-07-18 13:42:35 +0300

    sablier nft descriptor done

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,16 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
-require('dotenv').config()
-
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.26",
-  settings: {
-    viaIR: true,
-    optimizer: {
-      enabled: true,
-      runs: 10,
+  solidity: {
+    version: "0.8.26",
+    
+  ignition: {
+    requiredConfirmations: 1
+  },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          yulDetails: {
+            optimizerSteps: "u",
+          },
+        },
+      },
+      viaIR: true,
+
     },
   },
   networks: {
@@ -20,28 +31,27 @@ module.exports = {
     },
     opencampus: {
       url: "https://rpc.open-campus-codex.gelato.digital",
-      accounts: [`${process.env.PRIVATE_KEY}`]
-    }
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
   },
   etherscan: {
     apiKey: {
       // Is not required by blockscout. Can be any non-empty string
       sepolia: "abc",
-      opencampus:"abc"
+      opencampus: "abc",
     },
     customChains: [
       {
         network: "opencampus",
         chainId: 656476,
         urls: {
-          apiURL: "	https://opencampus-codex.blockscout.com/api",
-          browserURL: "	https://opencampus-codex.blockscout.com/",
-        }
-      }
-    ]
+          apiURL: "https://opencampus-codex.blockscout.com/api",
+          browserURL: "https://opencampus-codex.blockscout.com/",
+        },
+      },
+    ],
   },
   sourcify: {
-    enabled: false
-  }
+    enabled: false,
+  },
 };
-
