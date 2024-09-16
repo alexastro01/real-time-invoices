c8d05c5a32717e98f9e3151f29eb75a06d1ed9a1
Alex Astro <101653301+alexastro01@users.noreply.github.com>
2024-09-16 15:22:31 +0300

    clean import

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,14 +1,8 @@
 import Navbar from "@/components/Navbar";
-
-import HowItWorks from "@/components/HowItWorks";
-import Benefits from "@/components/Benefits";
 import Hero from "@/components/landing-page/Hero";
 import Features from "@/components/landing-page/Features";
-import { MarqueeDemo } from "@/components/landing-page/MarqueePeople";
 import PeopleShowcase from "@/components/landing-page/PeopleShowcase";
-import { MagicTweet } from "@/components/landing-page/MagicTweet";
 import FollowTheJourney from "@/components/landing-page/FollowTheJourney";
-import JoinWaitlistForm from "@/components/landing-page/JoinWaitlistForm";
 
 export default function Home() {
   return (
