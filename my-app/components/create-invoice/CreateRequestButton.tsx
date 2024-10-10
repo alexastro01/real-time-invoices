369e2302fbb6b6d184f92478a9c6804026150a99
Alex Astro <101653301+alexastro01@users.noreply.github.com>
2024-10-10 13:25:28 +0300

    add sepolia

--- my-app/components/create-invoice/CreateRequestButton.tsx
+++ my-app/components/create-invoice/CreateRequestButton.tsx
@@ -61,10 +61,10 @@ const CreateRequestButton: React.FC<CreateRequestButtonProps> = ({
 
     try {
       const web3SignatureProvider = new Web3SignatureProvider(walletClient);
-      //switch to gnosis for now
+      //switch to sepolia for now
       const requestClient = new RequestNetwork({
         nodeConnectionConfig: {
-          baseURL: "https://gnosis.gateway.request.network",
+          baseURL: "https://sepolia.gateway.request.network",
         },
         signatureProvider: web3SignatureProvider,
       });
@@ -113,7 +113,7 @@ const CreateRequestButton: React.FC<CreateRequestButtonProps> = ({
           expectedAmount,
           dueDate,
           chain,
-          gateway: 'gnosis'
+          gateway: 'sepolia'
         }),
       });
 
@@ -131,7 +131,7 @@ const CreateRequestButton: React.FC<CreateRequestButtonProps> = ({
 
     } catch (error: any) {
       console.log(error)
-      alert('Error : gnosis gateway | We are aware of it and we are working on it ! :) | Update time : 1 OCTOBER 2024')
+      alert('Error : sepolia gateway | We are aware of it and we are working on it ! :) | Update time : 1 OCTOBER 2024')
       setDialogMessage(`Error: ${error}`);
       setLoading(false)
     } finally {
